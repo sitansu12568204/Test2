219325b4078075271a6590f8f2d3237b2ffadb04
sitannsu <android.sitansu@gmail.com>
2016-06-16 23:31:43 +0530

    c

--- app/src/main/java/petactive/kkkkkkkkkkkkk/MainActivity.java
+++ app/src/main/java/petactive/kkkkkkkkkkkkk/MainActivity.java
@@ -2,6 +2,7 @@ package petactive.kkkkkkkkkkkkk;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        for (int i=0;i<55;i++) {
+            Log.d("2222222", "444444");
+        }
     }
 }
